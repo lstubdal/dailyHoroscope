30ada4727b8a8135801a66c2b5385f2ad1b3cd45
lstubdal <lotte.stubdal@gmail.com>
2022-03-15 09:15:51 +0100

    Project setup

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,8 +1,9 @@
 <template>
     <div class="home">
         <div class="home__title"> {{ title }}</div>
-        <div class="home__zodiac-signs">
-            zodiac grid here
+        
+        <div class="zodiac-board">
+            <button @click="fetchZodiacData"> click click</button>
         </div>
 
         <Footer />
@@ -16,12 +17,35 @@
     export default {
         data() {
             return {
-                title: 'DAILY HOROSCOPE'
+                title: 'DAILY HOROSCOPE',
+                zodiacName: '',
+                zodiacDate: '',
+                zodiacHoroscope: '',
+            }
+        },
+
+        computed: {
+            zodiacNames() {
+                return this.$store.getters.getZodiacNames;
             }
         },
 
         components: {
             Footer
+        },
+
+        methods: {
+            async fetchZodiacData() {   /* iterate through loop with zodiac name because the API only shows one zodiac sign at the time  */
+                /* zodiacNames.forEach(name => { */
+
+                    const url = '';
+                    const response = await fetch(url);
+                    const data = await response.json();
+
+                    console.log('SIGN: ', );
+
+              /*   }); */
+            }
         }
     }
 </script>
@@ -35,6 +59,7 @@
         width: 100vw;
         background-image: url('/images/background-home.jpg');
         background-size: cover;
+        padding: var(--super-large);
     }
 
     .home__title {
@@ -44,6 +69,14 @@
     }
 
     .zodiac-board {
+        display: grid;
+        grid-template-columns: repeat(4, 1fr);
+        gap: var(--grid-gap);
+    }
 
+    .zodiac-board__sign {
+        height: var(--zodiac-size);
+        width: var(--zodiac-size);
+        border: 2px solid white;
     }
 </style>
--- src/store.js
+++ src/store.js
@@ -1,5 +1,16 @@
 export default {
     state() {
-        
+        return {
+            zodiacNames: [
+                'aquarius', 'pisces', 'aries', 'taurus', 'gemini', 'cancer', 'leo', 
+                'virgo', 'libra', 'scorpio', 'saggitarius', 'capricorn'
+            ]
+        }
+    },
+
+    getters: {
+        getZodiacNames(state) {
+            return state.zodiacNames;
+        }
     }
 }
