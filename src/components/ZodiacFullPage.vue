3945405abf0f5635895ead470a715740433e2703
lstubdal <lotte.stubdal@gmail.com>
2022-03-15 12:45:01 +0100

    create fullZodiac page

--- src/components/ZodiacFullPage.vue
+++ src/components/ZodiacFullPage.vue
@@ -1,6 +1,20 @@
 <template>
-  <div class="zodiacPage">
-      
+  <!-- show horoscope based on users click -->
+  <div class="zodiacPage" v-if="correctSlug">
+    <h1 class="zodiacPage__title"> {{ zodiacSign.name }}</h1>
+    <img :src="zodiacSign.symbol" :alt="zodiacSign.name">
+    <p class="zodiacPage__date">{{ zodiacSign.dateRange}}</p>
+    <main>
+      <p class="horoscope"> {{ zodiacSign.description }}</p>
+
+      <section>
+        <div class="horoscope__information">
+          <span class="horoscope__luckyNumber"> {{ zodiacSign.luckyNumber}} Lucky number</span>
+          <span class="horoscope__mood"> {{ zodiacSign.mood }} Mood </span>
+          <span class="horoscope__compatibility"> {{ zodiacSign.compatibility }} Compatibility</span>
+        </div>
+      </section>
+    </main>
   </div>
 </template>
 
