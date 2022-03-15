c4b9f78374aa09c05c08127cb2080db59c524850
lstubdal <lotte.stubdal@gmail.com>
2022-03-15 09:49:55 +0100

    add zodiac name+symbol in store

--- src/store.js
+++ src/store.js
@@ -2,8 +2,18 @@ export default {
     state() {
         return {
             zodiacNames: [
-                'aquarius', 'pisces', 'aries', 'taurus', 'gemini', 'cancer', 'leo', 
-                'virgo', 'libra', 'scorpio', 'saggitarius', 'capricorn'
+                {sign: 'aquarius', symbol: '/images/aquarius.svg'},
+                {sign: 'pisces', symbol: '/images/pisces.svg'},
+                {sign: 'aries', symbol: '/images/aries.svg'},
+                {sign: 'taurus', symbol: '/images/taurus.svg'},
+                {sign: 'gemini', symbol: '/images/gemini.svg'},
+                {sign: 'cancer', symbol: '/images/cancer.svg'},
+                {sign: 'leo', symbol: '/images/leo.svg'},
+                {sign: 'virgo', symbol: '/images/virgo.svg'},
+                {sign: 'libra', symbol: '/images/libra.svg'},
+                {sign: 'scorpio', symbol: '/images/scorpio.svg'},
+                {sign: 'saggitarius', symbol: '/images/saggitarius.svg'},
+                {sign: 'capricorn', symbol: '/images/capricorn.svg'},
             ]
         }
     },
