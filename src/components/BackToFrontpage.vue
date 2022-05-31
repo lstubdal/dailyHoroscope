21eb5a51d703170b3c8df8431ae72d77b32113b4
lstubdal <lotte.stubdal@gmail.com>
2022-05-31 18:31:45 +0200

    Create component

---
+++ src/components/BackToFrontpage.vue
@@ -0,0 +1,31 @@
+<template>
+  <RouterLink :to="{ name: 'home' }" class="zodiacPage__back">
+      <img src="/images/back.svg" alt="back to homepage">
+      <p>Back to homepage</p>
+    </RouterLink>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    .zodiacPage__back {
+        position: absolute;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        padding: var(--padding-medium);
+        font-size: 1.5em;
+        cursor: pointer;
+        color: var(--light);
+        text-decoration: none;
+    }
+
+    .zodiacPage__back:hover {
+        text-decoration: underline;
+        color: var(--highlight);
+    }
+</style>
